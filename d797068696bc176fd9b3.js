223f90e352fc86f67456d4828a35df519797d2bb
DurckinaMilana <durckina.milana2016@yandex.ru>
2023-08-09 16:43:52 +0000

    371c9ae

---
+++ d797068696bc176fd9b3.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+// TODO: write code here
+import Board from "./board";
+import { rawPosition } from "./goblin";
+var board = new Board();
+board.draw();
+rawPosition();
